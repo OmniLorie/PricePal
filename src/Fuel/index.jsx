156f5a95a5abd6f2ffa839cf625e95252ab6996f
OmniLorie <Losibe27@gmail.com>
2024-08-19 16:11:49 +0000

    Update

--- src/Fuel/index.jsx
+++ src/Fuel/index.jsx
@@ -1,63 +1,84 @@
+import { useEffect, useState } from "react";
+import { apiCategoryByName } from "../services/categories";
+
+const headings = ["Company", "Petrol", "Diesel", "Premium"];
 const Fuel = () => {
-  return (
-    <div>
-      <div className="flex flex-col w-full px-8  md:w-[90%] mx-auto ">
-        <div className="flex justify-between px-16 mb-4 items-center">
-          <h4 className="font-bold text-gray-800 text-xl">
-            {props.category?.categoryName?.toUpperCase()}
-          </h4>
-        </div>
-        {/* ----------------Table Heading--------------------- */}
-        <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 shadow-md rounded-t-md p-4 font-semibold text-lg bg-gray-300">
-          {headings.map((heading, index) => {
-            return (
-              <p key={index}>
-                {heading} {index > 0 ? "GHC" : ""}
-              </p>
-            );
-          })}
-        </div>
-        <div className="border-y">
-          {props.category?.companies.map((company, index) => {
-            if (index > 2) return;
-            return (
-              <div
-                className={`grid text-sm text-gray-600 ${
-                  index % 2 !== 1 ? "bg-gray-100" : "bg-white"
-                } grid-cols-1 sm:grid-cols-4 gap-4 py-4 items-center px-5`}
-                key={index}
-              >
-                <div className="flex align-middle items-center gap-2">
-                  <img
-                    src={`https://savefiles.org/${company.logo}?shareable_link=329`}
-                    alt={company.companyName}
-                    className="size-6"
-                  />
-                  <p className="pl-1">{company.companyName}</p>
-                </div>
-                {["Petrol", "Diesel", "Premium"].map((productType) => {
-                  const product = company.products.find(
-                    (product) => product.productName === productType
-                  );
-                  return (
-                    <p key={productType} className="pl-1">
-                      {product ? product.price : "N/A"}
-                    </p>
-                  );
-                })}
-              </div>
-            );
-          })}{" "}
-        </div>
-        <div className="flex justify-end items-center w-full mt-6">
-          <Link to="/fuel" className="text-blue-500 underline">
-            Show more
-          </Link>
-        </div>
-      </div>
-      ); };
-    </div>
-  );
+  //   const [loading, setLoading] = useState(false);
+  //   const [data, setData] = useState(null);
+
+  //   const fetchCategory = async (categoryName) => {
+  //     setLoading(true);
+  //     try {
+  //       const res = await apiCategoryByName(categoryName);
+  //       console.log("data-->", res);
+  //       if (res.status === 200) {
+  //         setData(res.data);
+  //       }
+  //     } catch (error) {
+  //       console.log("Error Fetching category", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     fetchCategory("Fuel");
+  //   }, []);
+
+  return <div>feul</div>;
+  //   (
+  //     <div>
+  //       <div className="flex flex-col w-full px-8  md:w-[90%] mx-auto ">
+  //         <div className="flex justify-between px-16 mb-4 items-center">
+  //           <h4 className="font-bold text-gray-800 text-xl">
+  //             {data.category?.categoryName?.toUpperCase()}
+  //           </h4>
+  //         </div>
+  //         {/* ----------------Table Heading--------------------- */}
+  //         <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 shadow-md rounded-t-md p-4 font-semibold text-lg bg-gray-300">
+  //           {headings.map((heading, index) => {
+  //             return (
+  //               <p key={index}>
+  //                 {heading} {index > 0 ? "GHC" : ""}
+  //               </p>
+  //             );
+  //           })}
+  //         </div>
+  //         <div className="border-y">
+  //           {data.category?.companies.map((company, index) => {
+  //             if (index > 2) return;
+  //             return (
+  //               <div
+  //                 className={`grid text-sm text-gray-600 ${
+  //                   index % 2 !== 1 ? "bg-gray-100" : "bg-white"
+  //                 } grid-cols-1 sm:grid-cols-4 gap-4 py-4 items-center px-5`}
+  //                 key={index}
+  //               >
+  //                 <div className="flex align-middle items-center gap-2">
+  //                   <img
+  //                     src={`https://savefiles.org/${company.logo}?shareable_link=329`}
+  //                     alt={company.companyName}
+  //                     className="size-6"
+  //                   />
+  //                   <p className="pl-1">{company.companyName}</p>
+  //                 </div>
+  //                 {["Petrol", "Diesel", "Premium"].map((productType) => {
+  //                   const product = company.products.find(
+  //                     (product) => product.productName === productType
+  //                   );
+  //                   return (
+  //                     <p key={productType} className="pl-1">
+  //                       {product ? product.price : "N/A"}
+  //                     </p>
+  //                   );
+  //                 })}
+  //               </div>
+  //             );
+  //           })}{" "}
+  //         </div>
+  //         <div className="flex justify-end items-center w-full mt-6"></div>
+  //       </div>
+  //     </div>
+  //   );
 };
 
 export default Fuel;
